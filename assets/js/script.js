b8aaf8d4f80942eb03f9dd40c920f5f37d03e629
Mark Briscoe <mark@mcmw.co.uk>
2024-02-08 12:53:49 +0000

    feat: share to FaceBook

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,3 +144,14 @@ function getConfig() {
 
     return config;
 }
+
+var fbButton = document.getElementById('fb-share-button');
+var url = window.location.href;
+
+fbButton.addEventListener('click', function () {
+    window.open('https://www.facebook.com/sharer/sharer.php?u=' + url,
+        'facebook-share-dialog',
+        'width=800,height=600'
+    );
+    return false;
+});
--- assets/js/start.js
+++ assets/js/start.js
@@ -62,3 +62,14 @@ function setConfig(questionCount, category, difficulty, type) {
     sessionStorage.setItem('difficulty', difficulty);
     sessionStorage.setItem('type', type);
 }
+
+var fbButton = document.getElementById('fb-share-button');
+var url = window.location.href;
+
+fbButton.addEventListener('click', function () {
+    window.open('https://www.facebook.com/sharer/sharer.php?u=' + url,
+        'facebook-share-dialog',
+        'width=800,height=600'
+    );
+    return false;
+});
